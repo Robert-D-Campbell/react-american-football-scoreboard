6ddaa9e63075e2e6d64635d505bf63e41ef20d45
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2019-10-08 13:24:33 -0700

    stretch goal(reset quarter)

--- src/Quarter.js
+++ src/Quarter.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 
 const Quarter = () => {
-  //   function quarterHandler(quarter) {
-  //     if (quarter == 4) {
-  //       quarter = 0;
-  //     } else {
-  //       quarter + 1;
-  //     }
-  //     return;
-  //   }
   const [quarter, setQuarter] = useState(0);
+
+  function quarterHandler() {
+    if (quarter == 4) {
+      setQuarter(0);
+    } else {
+      setQuarter(quarter + 1);
+    }
+    return;
+  }
 
   return (
     <div className="quarter">
       <h3 className="quarter__title">Quarter</h3>
-      <a onClick={() => setQuarter(quarter + 1)}>
+      <a onClick={() => quarterHandler()}>
         <div className="quarter__value">{quarter}</div>
       </a>
     </div>
